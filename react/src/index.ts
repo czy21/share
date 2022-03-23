e5a1c6a9fb6d0c88007863e4f98da8e85c9fb478
Bruce Chen <805899926@qq.com>
2022-03-23 22:01:37 +0800

    #

--- react/src/index.ts
+++ react/src/index.ts
@@ -1,5 +1,5 @@
 export {default as Tree} from './tree'
 export {default as Filter} from './filter'
-export {default as Table} from './table'
+export {default as Table, OperationRender} from './table'
 export {default as Modal} from './modal'
 export {default as Button} from './button'
