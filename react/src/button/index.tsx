7f2d42e51e6173984bb1dcc58d792c0d6ea2ffb7
Bruce Chen <805899926@qq.com>
2022-03-25 01:55:49 +0800

    #

--- react/src/button/index.tsx
+++ react/src/button/index.tsx
@@ -7,6 +7,6 @@ export interface ButtonProps {
 
 
 const Index: React.FC<ButtonProps> = (props: ButtonProps) => {
-    return (<div><Button>{props.label}</Button><p>bushidaoo</p></div>)
+    return (<Button>{props.label}</Button>)
 }
 export default Index;
