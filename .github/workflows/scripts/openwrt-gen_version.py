e3b2bd79b511e45bc3369a0288a3a9a5e67b55c5
Bruce Chen <805899926@qq.com>
2025-09-30 20:34:57 +0800

    add pkg

--- .github/workflows/scripts/openwrt-gen_version.py
+++ .github/workflows/scripts/openwrt-gen_version.py
@@ -1,30 +1,7 @@
 import argparse
 import json
 import pathlib
-
-
-def gen_overview(release_name, release_dir):
-    overview_obj = {
-        "release": release_name,
-        "profiles": []
-    }
-    targets_obj = {}
-    overview_file = release_dir.joinpath(".overview.json")
-    targets_file = release_dir.joinpath(".targets.json")
-    for p in release_dir.rglob("profiles.json"):
-        profile_obj = json.loads(p.read_text())
-        profile_obj_profiles: dict[str, dict] = profile_obj["profiles"]
-        for pk, pv in profile_obj_profiles.items():
-            t_overview = {
-                "id": pk,
-                "titles": pv.get("titles"),
-                "target": profile_obj["target"]
-            }
-            overview_obj["profiles"].append(t_overview)
-        targets_obj[profile_obj["target"]] = profile_obj["arch_packages"]
-    overview_file.write_text(json.dumps(overview_obj))
-    targets_file.write_text(json.dumps(targets_obj))
-
+import requests
 
 if __name__ == '__main__':
 
@@ -41,7 +18,6 @@ if __name__ == '__main__':
 
     versions = []
     for t in sorted(filter(lambda f: f.is_dir(), artifact_releases_dir.glob("[0-9]*")), reverse=True, key=lambda x: x.name):
-        gen_overview(t.name, t)
         versions.append(t.name)
 
     stable_versions = list(filter(lambda v: 'rc' not in v, versions))
@@ -55,6 +31,3 @@ if __name__ == '__main__':
     if upcoming_versions:
         artifact_versions_obj['upcoming_version'] = upcoming_versions[0]
     artifact_versions_file.write_text(json.dumps(artifact_versions_obj))
-
-    # snapshot
-    gen_overview("SNAPSHOT", artifact_snapshots_dir)
--- .github/workflows/scripts/openwrt-get_targets.py
+++ .github/workflows/scripts/openwrt-get_targets.py
@@ -6,29 +6,35 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-t','--targets', nargs="+", default=[])
 args: argparse.Namespace = parser.parse_args()
 
-targets = []
+targets = set()
 subtargets = []
 archs = []
 
+# overview_path = pathlib.Path(__file__).joinpath("../../../../overview.json").resolve()
+# profiles_objs = json.loads(overview_path.read_text()).get('profiles') if overview_path.exists() else []
+profiles_objs = []
 packages_path = pathlib.Path(__file__).joinpath("../../../../packages.json").resolve()
-if packages_path.exists():
-    packages_objs = json.loads(packages_path.read_text())
-    archs.extend([p['name'] for p in packages_objs if p['type'] == 'directory'])
+packages_objs = json.loads(packages_path.read_text()) if packages_path.exists() else []
+
+archs.extend([p['name'] for p in packages_objs if p['type'] == 'directory'])
 
 config_path = pathlib.Path(__file__).joinpath("../../../../config").resolve()
 global_profiles = config_path.joinpath("profiles.json")
-config_profiles = list(filter(lambda t: t != global_profiles,config_path.rglob("**/*profiles.json")))
-if args.targets:
-    config_profiles = list(filter(lambda t: t.exists(),[config_path.joinpath(t).joinpath('profiles.json') for t in args.targets]))
-for p in config_profiles:
-    profile_obj = json.loads(p.read_text())
-    profile_target = profile_obj.get("target")
+config_profiles = [json.loads(t.read_text()) for t in filter(lambda t: t != global_profiles,config_path.rglob("**/*profiles.json"))]
 
+profiles_objs = list({t.get('target'): {"target": t.get('target')} for t in profiles_objs}.values())
+profiles_objs = profiles_objs if profiles_objs else config_profiles
+
+if args.targets:
+    profiles_objs = list(filter(lambda t: t.get('target') in args.targets,profiles_objs))
+
+for p in profiles_objs:
+    profile_target = p.get("target")
     target = profile_target.split("/")[0]
     subtarget = profile_target.split("/")[1]
-    targets.append(target)
+    targets.add(target)
     subtargets.append({"target": target, "subtarget": subtarget})
 
-print("targets={0}".format(json.dumps(targets)))
+print("targets={0}".format(json.dumps(list(targets))))
 print("targets_subtargets={0}".format(json.dumps(subtargets)))
 print("archs={0}".format(json.dumps(archs)))
