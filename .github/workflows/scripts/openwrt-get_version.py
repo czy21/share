960d86e28d209a9043e8a4ba3dbbb04c2e13d0cb
Bruce Chen <805899926@qq.com>
2026-03-06 09:14:35 +0800

    sort versions_list

--- .github/workflows/scripts/openwrt-get_version.py
+++ .github/workflows/scripts/openwrt-get_version.py
@@ -1,6 +1,25 @@
 import argparse
 import json
 import pathlib
+import re
+
+def version_key(v):
+    m = re.match(r"(\d+)\.(\d+)\.(\d+)(?:-rc(\d+))?", v)
+    major, minor, patch, rc = m.groups()
+
+    major = int(major)
+    minor = int(minor)
+    patch = int(patch)
+
+    # 正式版排 rc 前面
+    if rc is None:
+        rc_flag = 1
+        rc_num = 0
+    else:
+        rc_flag = 0
+        rc_num = int(rc)
+
+    return (major, minor, patch, rc_flag, rc_num)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-b','--branch')
@@ -8,7 +27,8 @@ args: argparse.Namespace = parser.parse_args()
 
 versions_file = pathlib.Path(__file__).joinpath("../../../../.versions.json").resolve()
 versions_json = json.loads(versions_file.read_text()) if versions_file.exists() else {}
+versions_list = versions_json.get('versions_list',[])
 
-version = next((v for v in versions_json['versions_list'] if v.startswith(args.branch)), f"{args.branch}.0")
+version = max((v for v in versions_list if v.startswith(args.branch)),key=version_key,default=f"{args.branch}.0")
 
 print(version)
